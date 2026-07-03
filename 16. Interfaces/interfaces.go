d3cebdef2e08192751052cbc968173902b7b345e
Vinicius-CRodrigues <vinicius.cardoso.vcr@gmail.com>
2026-07-03 09:55:16 -0300

    alterando o conteúdo de interfaces

--- 16. Interfaces/interfaces.go
+++ 16. Interfaces/interfaces.go
@@ -1,41 +1,45 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-type rectangle struct {
-	altura  float64
-	largura float64
+type atacante interface {
+	atacar() string
 }
 
-type circle struct {
-	raio float64
+type guerreiro struct {
+	nome  string
+	forca int
 }
 
-// Aqui temos assinaturas de métodos.
-type forma interface {
-	// Crio o método área que retorna um float64.
-	area() float64
+type mago struct {
+	nome       string
+	forcaMagia int
 }
 
-func escreverArea(f forma) {
-	fmt.Printf("A área da forma é %.2f \n", f.area())
+func (g guerreiro) atacar() string {
+	dano := 2 * g.forca
+	return fmt.Sprintf("%s atacou com a força %d", g.nome, dano)
+
 }
 
-// Para que eu consiga usar o método da interface, preciso criar o método do struct.
-func (rec rectangle) area() float64 {
-	return rec.altura * rec.largura
+func (m mago) atacar() string {
+	danoMago := 2 * m.forcaMagia
+
+	return fmt.Sprintf("%s atacou com a força %d", m.nome, danoMago)
+
 }
 
-func (cir circle) area() float64 {
-	return math.Pi * math.Pow(cir.raio, 2)
+func descreverPersonagem(atack atacante) {
+	fmt.Println(atack.atacar())
+
 }
 
 func main() {
-	retangulo := rectangle{10, 10}
-	circulo := circle{10}
-	escreverArea(retangulo)
-	escreverArea(circulo)
+	userGuerreiro := guerreiro{"Barbaro", 10}
+
+	userMago := mago{"Veigar", 1000}
+
+	descreverPersonagem(userGuerreiro)
+	descreverPersonagem(userMago)
+
 }
